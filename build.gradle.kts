4884c01144fb7042e17c7f8a5d2ef1528e6fae73
Fast-IQ <fire-burn@yandex.ru>
2025-09-15 09:17:12 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.3.5.RELEASE"
+    id("org.springframework.boot") version "2.7.18"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     id("org.jetbrains.kotlin.plugin.jpa") version "1.4.32"
     id("org.sonarqube") version "6.3.1.5724"
